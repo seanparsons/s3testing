1c4c9362a3c1ed2fa3f062af9bbb04836d15e0b6
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2023-02-01 11:13:43 +0000

    It's great.

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -25,7 +25,7 @@ export var storyboard = (
         left: 1036,
         top: 128,
       }}
-      data-label='My App'
+      data-label='My App Is Great'
     >
       <App style={{}} />
     </Scene>
